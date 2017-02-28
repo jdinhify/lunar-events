2fe633f1c87ba71a5662024e988a02c0f3e23ac4
JD <me@jdinh.life>
2017-02-28 15:46:30 +1100

    login error

--- src/event/user-login/index.js
+++ src/event/user-login/index.js
@@ -10,12 +10,13 @@ class UserLogin extends Component {
     super(props)
     this.state = {
       id: '',
-      password: ''
+      password: '',
+      error: false
     }
   }
 
   handleInputChange = name => e =>
-    this.setState({[name]: e.target.value})
+    this.setState({[name]: e.target.value, error: false})
 
   handleSubmit = e => {
     const { signinUser } = this.props
@@ -30,12 +31,13 @@ class UserLogin extends Component {
       .then((response) => {
         window.localStorage.setItem('graphcoolToken', response.data.signinUser.token)
       })
-      .catch((e) => {
-        console.error(e)
-      })
       .then(() =>
         window.location.reload()
       )
+      .catch((e) => {
+        console.error(e)
+        this.setState({error: true})
+      })
   }
 
   render () {
@@ -57,6 +59,10 @@ class UserLogin extends Component {
               placeholder='password'
               onChange={this.handleInputChange('password')} />
           </div>
+          {this.state.error
+          ? <div className='error row justify-center'><span>Error!</span></div>
+          : undefined
+          }
           <div className='row justify-center'>
             <button>Login</button>
           </div>
