4aa4a1aef59a076b60aef73b241c508d78db6a19
JD <jdinhify@gmail.com>
2020-07-08 10:37:03 +1000

    feat: ✨  add auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,51 @@
 import React, { useState } from 'react'
 import { EventList } from '../src/components/event-list'
 import { NewEvent } from '../src/components/new-event'
+import { Auth } from '../src/components/auth'
 
-export default () => {
+const Home = () => {
   const [year, setYear] = useState(String(new Date().getFullYear()))
 
   const years = [...Array(8).keys()].map(
     (number) => parseInt(year, 10) - 4 + number,
   )
   return (
-    <div>
+    <Auth>
       <NewEvent />
       <div className="row noprint">
         <div className="col-4-6">
           <span>Năm hiển thị: &nbsp;&nbsp;</span>
           <select value={year} onChange={(e) => setYear(e.target.value)}>
             {years.map((value) => (
-              <option value={value}>{value}</option>
+              <option key={value} value={value}>
+                {value}
+              </option>
             ))}
           </select>
         </div>
-        <div className="col-2-6 text-align-right">
-          <button onClick={() => window.print()}>In</button>
+        <div className="col-2-6 btn-print">
+          <button onClick={() => window.print()}>
+            <img
+              className="icon-print"
+              src="/icon-print.png"
+              alt="printer icon"
+            />{' '}
+            <span>In</span>
+          </button>
         </div>
       </div>
       <EventList year={year} />
-    </div>
+      <style jsx>{`
+        .btn-print {
+          display: flex;
+          justify-content: flex-end;
+        }
+        .icon-print {
+          height: 15px;
+        }
+      `}</style>
+    </Auth>
   )
 }
+
+export default Home
---
+++ src/components/auth.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import {
+  AmplifyAuthenticator,
+  AmplifySignIn,
+  AmplifySignUp,
+} from '@aws-amplify/ui-react'
+import { AuthState, AuthStateHandler } from '@aws-amplify/ui-components'
+import '../integration/aws-config'
+import { useRouter } from 'next/router'
+import { Auth as AmplifyAuth } from 'aws-amplify'
+
+export const Auth = ({ children }) => {
+  const { query } = useRouter()
+  const [state, setState] = useState<{ authState: AuthState }>({
+    authState: AuthState.Loading,
+  })
+
+  const handleAuthStateChange: AuthStateHandler = (nextAuthState) =>
+    setState({ authState: nextAuthState })
+
+  return state.authState === AuthState.SignedIn ||
+    state.authState === AuthState.VerifyContact ? (
+    <>
+      <div className="sign-out-wrapper">
+        <button onClick={() => AmplifyAuth.signOut()}>Đăng xuất</button>
+      </div>
+      {children}
+
+      <style jsx>{`
+        .sign-out-wrapper {
+          display: flex;
+          justify-content: flex-end;
+          padding: 1rem;
+        }
+      `}</style>
+    </>
+  ) : (
+    <div className="wrapper">
+      <AmplifyAuthenticator handleAuthStateChange={handleAuthStateChange}>
+        <AmplifySignIn
+          submitButtonText="Đăng nhập"
+          hideSignUp={query.showSignUp !== '1'}
+          slot="sign-in"
+          formFields={[{ type: 'username' }, { type: 'password', hint: '' }]}
+          headerText=""
+        />
+        <AmplifySignUp
+          slot="sign-up"
+          formFields={[
+            {
+              type: 'username',
+            },
+            {
+              type: 'password',
+            },
+          ]}
+        />
+      </AmplifyAuthenticator>
+
+      <style jsx>{`
+        .wrapper {
+          display: flex;
+          justify-content: center;
+          height: 100vh;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  )
+}
